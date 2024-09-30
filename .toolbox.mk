e69ac23f5682a1de82d6b35739a736afc94980ea
bakito <github@bakito.ch>
2024-09-30 23:50:32 +0200

    update tools

---
+++ .toolbox.mk
@@ -0,0 +1,58 @@
+## toolbox - start
+## Generated with https://github.com/bakito/toolbox
+
+## Current working directory
+TB_LOCALDIR ?= $(shell which cygpath > /dev/null 2>&1 && cygpath -m $$(pwd) || pwd)
+## Location to install dependencies to
+TB_LOCALBIN ?= $(TB_LOCALDIR)/bin
+$(TB_LOCALBIN):
+	mkdir -p $(TB_LOCALBIN)
+
+## Tool Binaries
+TB_GINKGO ?= $(TB_LOCALBIN)/ginkgo
+TB_GO_BUILDER_GENERATOR ?= $(TB_LOCALBIN)/go-builder-generator
+TB_GOLANGCI_LINT ?= $(TB_LOCALBIN)/golangci-lint
+TB_GORELEASER ?= $(TB_LOCALBIN)/goreleaser
+
+## Tool Versions
+TB_GINKGO_VERSION ?= v2.20.2
+TB_GO_BUILDER_GENERATOR_VERSION ?= v1.8.5
+TB_GOLANGCI_LINT_VERSION ?= v1.61.0
+TB_GORELEASER_VERSION ?= v2.3.2
+
+## Tool Installer
+.PHONY: tb.ginkgo
+tb.ginkgo: $(TB_GINKGO) ## Download ginkgo locally if necessary.
+$(TB_GINKGO): $(TB_LOCALBIN)
+	test -s $(TB_LOCALBIN)/ginkgo || GOBIN=$(TB_LOCALBIN) go install github.com/onsi/ginkgo/v2/ginkgo@$(TB_GINKGO_VERSION)
+.PHONY: tb.go-builder-generator
+tb.go-builder-generator: $(TB_GO_BUILDER_GENERATOR) ## Download go-builder-generator locally if necessary.
+$(TB_GO_BUILDER_GENERATOR): $(TB_LOCALBIN)
+	test -s $(TB_LOCALBIN)/go-builder-generator || GOBIN=$(TB_LOCALBIN) go install github.com/kilianpaquier/go-builder-generator/cmd/go-builder-generator@$(TB_GO_BUILDER_GENERATOR_VERSION)
+.PHONY: tb.golangci-lint
+tb.golangci-lint: $(TB_GOLANGCI_LINT) ## Download golangci-lint locally if necessary.
+$(TB_GOLANGCI_LINT): $(TB_LOCALBIN)
+	test -s $(TB_LOCALBIN)/golangci-lint || GOBIN=$(TB_LOCALBIN) go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(TB_GOLANGCI_LINT_VERSION)
+.PHONY: tb.goreleaser
+tb.goreleaser: $(TB_GORELEASER) ## Download goreleaser locally if necessary.
+$(TB_GORELEASER): $(TB_LOCALBIN)
+	test -s $(TB_LOCALBIN)/goreleaser || GOBIN=$(TB_LOCALBIN) go install github.com/goreleaser/goreleaser@$(TB_GORELEASER_VERSION)
+
+## Reset Tools
+.PHONY: tb.reset
+tb.reset:
+	@rm -f \
+		$(TB_LOCALBIN)/ginkgo \
+		$(TB_LOCALBIN)/go-builder-generator \
+		$(TB_LOCALBIN)/golangci-lint \
+		$(TB_LOCALBIN)/goreleaser
+
+## Update Tools
+.PHONY: tb.update
+tb.update: tb.reset
+	toolbox makefile -f $(TB_LOCALDIR)/Makefile \
+		github.com/onsi/ginkgo/v2/ginkgo \
+		github.com/kilianpaquier/go-builder-generator/cmd/go-builder-generator \
+		github.com/golangci/golangci-lint/cmd/golangci-lint \
+		github.com/goreleaser/goreleaser
+## toolbox - end
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/bakito/go-awtrix
 
-go 1.22.2
+go 1.23.1
 
 require (
 	github.com/go-resty/resty/v2 v2.15.3
@@ -12,10 +12,10 @@ require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/google/pprof v0.0.0-20240827171923-fa2c70bbbfe5 // indirect
-	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
-	golang.org/x/tools v0.24.0 // indirect
+	github.com/google/pprof v0.0.0-20240929191954-255acd752d31 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/tools v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
